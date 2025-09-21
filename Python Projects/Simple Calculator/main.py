a044b871b4becb6c6da71c7c443312883a437bdd
Paa-Kwasi-04 <paakwasi.owusu2023@gmail.com>
2025-09-21 11:59:26 +0000

    mods:used sys lib instead of argparse

--- Python Projects/Simple Calculator/main.py
+++ Python Projects/Simple Calculator/main.py
@@ -1,21 +1,26 @@
-import argparse
+import sys
 from ShuntingYardAlgorithm import ShuntingYardAlgo
 from postfixEval import PostfixEval
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Calculates Infix Expressions')
-    parser.add_argument(
-        'expression', help='Infix expression to evaluate (use quotes)')
-    args = parser.parse_args()
+    if len(sys.argv) > 1:
+        expression = " ".join(sys.argv[1:])
+    else:
+        expression = input("Enter an infix expression: ")
+    try:
+        SYA = ShuntingYardAlgo()
+        PFE = PostfixEval()
+        postfix_exp = SYA.shunting_yard(expression)
+        result = PFE.postfix_eval(postfix_exp)
+        print(f"Infix: {expression}")
+        print(f"Postfix: {postfix_exp}")
+        print(f"Result: {result}")
+    except KeyboardInterrupt:
+        print("\nCalculation cancelled by user")
+    except Exception as e:
+        print(e)
 
-    SYA = ShuntingYardAlgo()
-    PFE = PostfixEval()
-    postfix_exp = SYA.shunting_yard(args.expression)
-    result = PFE.postfix_eval(postfix_exp)
-    print(f"Infix: {args.expression}")
-    print(f"Postfix: {postfix_exp}")
-    print(f"Result: {result}")
 
 
 if __name__ == '__main__':
